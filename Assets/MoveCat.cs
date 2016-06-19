c41f8c4d23b5701c60941e302172aab78fba96f1
propakamda <kam.jenny@Gmail.com>
2016-06-18 17:52:19 -0700

    spacecats

---
+++ Assets/MoveCat.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCat : MonoBehaviour 
+{
+    Rigidbody catBox;
+    public float force = 10f;
+    public float rotate = 10f;
+    private void Start()
+    {
+        catBox = GetComponent<Rigidbody>();
+
+        catBox.AddForce(Random.Range(1, force), Random.Range(1, force), Random.Range(1, force));
+        catBox.AddTorque(Random.Range(1, rotate), Random.Range(1, rotate), Random.Range(1, rotate));
+    }
+
+
+
+}
---
+++ Assets/catbox/README.txt
@@ -0,0 +1,5 @@
+Site : www.suriyun.com
+----------------------------------------------
+Shader by Unitychan! 
+Please read the Unity chan license.
+http://unity-chan.com/
